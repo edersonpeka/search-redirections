0f55ace8df17d14b67c7138cddfe32edb5274ecf
edersonpeka <edersonpeka@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2020-01-02 04:49:17 +0000

    Feature: redirect empty search results
    git-svn-id: http://plugins.svn.wordpress.org/search-redirections/trunk@2220747 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- search-redirections.php
+++ search-redirections.php
@@ -4,7 +4,7 @@ Plugin Name: Search Redirections
 Plugin URI: https://ederson.peka.nom.br
 Description: Create redirect rules for any given search terms.
 Author: Ederson Peka
-Version: 0.1.2
+Version: 0.1.3
 Author URI: https://profiles.wordpress.org/edersonpeka/
 Text Domain: search-redirections
 */
@@ -14,7 +14,7 @@ if ( !class_exists( 'search_redirections' ) ) :
 class search_redirections {
 
     // Init
-    function init() {
+    public static function init() {
         // Internationalization
         load_plugin_textdomain( 'search-redirections', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
         // Hooking into admin's menus
@@ -25,10 +25,14 @@ class search_redirections {
         add_filter( 'request', array( __CLASS__, 'modify_search_term' ) );
         // Hooking into admin's screens
         add_action( 'admin_init', array( __CLASS__, 'admin_init' ) );
+        // Hooking into wp (after get posts)
+        add_action( 'wp', array( __CLASS__, 'after_get_posts' ) );
+        // Halt the main query in the case of an empty search 
+        add_filter( 'posts_search', array( __CLASS__, 'halt_query' ), 10, 2 );
     }
 
     // Creates options using Settings API
-    function options_init() {
+    public static function options_init() {
         // One settings section...
      	add_settings_section(
 		    'search_redirections_section',
@@ -36,7 +40,7 @@ class search_redirections {
 		    array( __CLASS__, 'settings_description' ),
 		    'search_redirections_options'
 	    );
-	    // ...with only one settings field
+	    // ...with only two settings fields
 	    add_settings_field(
 		    'search_redirections_rules',
 		    __( 'Rules', 'search-redirections' ),
@@ -44,18 +48,27 @@ class search_redirections {
 		    'search_redirections_options',
 		    'search_redirections_section',
 		    array( 'slug' => 'search_redirections_rules', 'label_for' => 'search_redirections_rules' )
-	    );
+        );
         register_setting( 'search_redirections_options', 'search_redirections_rules' );
+        add_settings_field(
+		    'search_redirections_empty_search_dest',
+		    __( 'Redirect empty search results', 'search-redirections' ),
+		    array( __CLASS__, 'empty_field' ),
+		    'search_redirections_options',
+		    'search_redirections_section',
+		    array( 'slug' => 'search_redirections_empty_search_dest', 'label_for' => 'empty_search_dest' )
+	    );
+        register_setting( 'search_redirections_options', 'search_redirections_empty_search_dest' );
 
         // Create "settings" link for this plugin on plugins list
         add_filter( 'plugin_action_links', array( __CLASS__, 'settings_link' ), 10, 2 );
     }
-    function settings_description() {
+    public static function settings_description() {
         // void
     }
     
     // Returns array of saved rules (terms and destination)
-    function get_rules() {
+    public static function get_rules() {
         // Get saved rules
         $rules = get_option( 'search_redirections_rules' );
         // If empty, initializes array
@@ -76,7 +89,7 @@ class search_redirections {
     }
 
     // Build form fields for rules
-    function rules_field() {
+    public static function rules_field() {
         // Saved rules
         $rules = call_user_func( array( __CLASS__, 'get_rules' ) );
         $group_names = array_unique( array_map( function ( $rule ) {
@@ -163,13 +176,40 @@ class search_redirections {
         <?php
     }
 
-    function add_options_page() {
+    // Build form field for Redirect empty search results rule
+    public static function empty_field() {
+        // Saved rules
+        $empty_search_dest = get_option( 'search_redirections_empty_search_dest' );
+        ?>
+        <h4>
+            <label for="empty_search_dest">
+                <?php _e( 'Redirect empty search results', 'search-redirections' ); ?>
+            </label>
+        </h4>
+        <table class="form-table">
+            <tbody>
+                <tr>
+                    <th scope="row">
+                        <label for="empty_search_dest">
+                            <?php _e( 'Redirect empty search results', 'search-redirections' ); ?>
+                        </label>
+                    </th>
+                    <td>
+                        <input name="search_redirections_empty_search_dest" type="text" id="empty_search_dest" value="<?php echo esc_attr( $empty_search_dest ); ?>" class="regular-text" />
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+        <?php
+    }
+
+    public static function add_options_page() {
         $_page = add_options_page( __( 'Search Redirections', 'search-redirections' ), __( 'Search Redirections', 'search-redirections' ), 'edit_theme_options', 'search_redirections_options', array( __CLASS__, 'render_options_page' ) );
         add_action( 'admin_print_styles-' . $_page, array( __CLASS__, 'admin_styles' ) );
         add_action( 'admin_print_scripts-' . $_page, array( __CLASS__, 'admin_scripts' ) );
     }
 
-    function render_options_page() {
+    public static function render_options_page() {
         ?>
         <div class="wrap">
 
@@ -189,7 +229,7 @@ class search_redirections {
     }
 
     // On search request
-    function modify_search_term( $request_vars ) {
+    public static function modify_search_term( $request_vars ) {
         // Backup environment locale
         if ( function_exists( 'iconv' ) ) $orig_locale = setlocale( LC_CTYPE, 0 );
         // Redir rules
@@ -209,7 +249,7 @@ class search_redirections {
                         // ...redirects!
                         header( 'Location: ' . $rule['dest'] );
                         // (And that's all, folks)
-                        die();
+                        exit();
                     endif;
                 endif;
             endforeach;
@@ -220,7 +260,7 @@ class search_redirections {
     }
     
     // Prepare string for comparison: lowercase, no spaces, etc.
-    function reduce_term( $term ) {
+    public static function reduce_term( $term ) {
         // Lowercase and no spaces
         $reduced = str_replace( ' ', '', mb_strtolower( $term ) );
         // If "iconv" is present...
@@ -235,7 +275,7 @@ class search_redirections {
 
     // Add Settings link to plugins - code from GD Star Ratings
     // (as seen in http://www.whypad.com/posts/wordpress-add-settings-link-to-plugins-page/785/ )
-    function settings_link( $links, $file ) {
+    public static function settings_link( $links, $file ) {
         $this_plugin = plugin_basename(__FILE__);
         if ( $file == $this_plugin ) {
             $settings_link = '<a href="' . admin_url( 'options-general.php?page=search_redirections_options' ) . '">' . __( 'Settings', 'search-redirections' ) . '</a>';
@@ -244,7 +284,7 @@ class search_redirections {
         return $links;
     }
 
-    function admin_init() {
+    public static function admin_init() {
         $p_dir = WP_PLUGIN_DIR . '/' . dirname( plugin_basename( __FILE__ ) ) . '/';
         $p_url = WP_PLUGIN_URL . '/' . dirname( plugin_basename( __FILE__ ) ) . '/';
         wp_register_style(
@@ -260,10 +300,10 @@ class search_redirections {
             filemtime( $p_dir . 'js/admin.js' )
         );
     }
-    function admin_styles() {
+    public static function admin_styles() {
         wp_enqueue_style( 'search-redirections-admin-css' );
     }
-    function admin_scripts() {
+    public static function admin_scripts() {
         wp_localize_script( 'search-redirections-admin-script', 'search_redirections', array(
             'remove_group' => __( 'Remove group', 'search-redirections' ),
             'add_rule' => __( 'Add rule', 'search-redirections' ),
@@ -272,6 +312,26 @@ class search_redirections {
             'confirm_unload' => __( 'Your unsaved data will be lost. Are you sure?', 'search-redirections' ),
         ) );
         wp_enqueue_script( 'search-redirections-admin-script', false, array( 'jquery' ) );
+    }
+
+    public static function after_get_posts() {
+        // Saved rules
+        $empty_search_dest = get_option( 'search_redirections_empty_search_dest' );
+        if ( $empty_search_dest ) {
+            global $wp_query;
+            if ( $wp_query->is_search() && $wp_query->is_main_query() ) {
+                if ( !$wp_query->post_count ) {
+                    header( 'Location: ' . $empty_search_dest );
+                    exit();
+                }
+            }
+        }
+    }
+
+    public static function halt_query( $search,  $q ) {
+        if( ! is_admin() && empty( $search ) && $q->is_search() && $q->is_main_query() )
+            $search .= ' AND 0 = 1 ';
+        return $search;
     }
 }
 
